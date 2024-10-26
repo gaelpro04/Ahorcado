e34a6334d491539dc631e246bf7626a061fd2da2
Gael <gael.lopez22@uabc.edu.mx>
2024-10-26 15:19:51 -0700

    avance

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        Ahorcado ahorcado = new Ahorcado(40,2);
-        ahorcado.jugar();
+        new TestInterfaz();
     }
 }
